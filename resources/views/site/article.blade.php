98e4bd58955541517ab8ed97665ec2d399cfb9cc
kareem22T <m.othman@clicksegypt.net>
2024-07-30 17:21:28 +0300

    edits

--- resources/views/site/article.blade.php
+++ resources/views/site/article.blade.php
@@ -109,7 +109,7 @@
         $more_visited = App\Models\Visit::with(['article' => function ($query) {
             $query->where('isDraft', false);
         }])
-        ->where('created_at', '>=', Carbon\Carbon::now()) // Filter visits from the last two days
+        ->where('created_at', '>=', Carbon\Carbon::now()->subDays(1)) // Filter visits from the last two days
         ->orderBy('total_visits', 'desc') // Order by the total visits
         ->take(5)
         ->get();
